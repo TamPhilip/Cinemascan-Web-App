4e440c174016b9312a2be13348fa65257da4644d
TamPhilip <philip.tam.code@gmail.com>
2019-04-04 23:44:05 -0400

    Added Dynamic Prediction

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, Response, request, render_template
+from flask import Flask, Response, request, render_template, jsonify
 import re
-from keras.preprocessing.sequence import pad_sequences
 import pickle
 from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
 import json
-from tensorflow import get_default_graph
 
-graph = get_default_graph()
 action_model = load_model('resources/n_most/action_model.h5')
 adventure_model = load_model('resources/n_most/adventure_model.h5')
 comedy_model = load_model('resources/n_most/comedy_model.h5')
@@ -16,19 +14,49 @@ mystery_model = load_model('resources/n_most/mystery_model.h5')
 romance_model = load_model('resources/n_most/romance_model.h5')
 thriller_model = load_model('resources/n_most/thriller_model.h5')
 
+models = {"Action": action_model,
+          "Adventure": adventure_model,
+          'Comedy': comedy_model,
+          "Crime": crime_model,
+          "Family": family_model,
+          "Mystery": mystery_model,
+          "Romance": romance_model,
+          "Thriller": thriller_model}
+
+del action_model
+del adventure_model
+del comedy_model
+del crime_model
+del family_model
+del mystery_model
+del romance_model
+del thriller_model
+
+for genre, model in models.items():
+    model._make_predict_function()
+
+
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def main():
      return render_template('main.html')
 
-@app.route('/', methods = ['POST'])
+@app.route('/submit', methods = ['GET'])
 def submit():
-    text = request.form['text']
+    text = request.args.get('text', "", type=str)
     clean = [re.sub(r"[^a-zA-Z0-9]+", ' ', k) for k in text.lower().split("\n")]
     x = tokenize(clean)
     tags, results = predict(x)
-    return render_template("main.html", tags=tags, data=results, text=text)
+    message = {
+        'status': 200,
+        'message': 'OK',
+        'result': results,
+        'tags': tags
+    }
+    toReturn = jsonify(message)
+    toReturn.status_code = 200
+    return toReturn
 
 @app.route('/tokenizer')
 def run_tokenizer():
@@ -53,24 +81,18 @@ def tokenize(text):
         return x
 
 def predict(sequence):
-    models = {"Action": action_model,
-              "Adventure": adventure_model,
-              'Comedy': comedy_model,
-              "Crime": crime_model,
-              "Family": family_model,
-              "Mystery": mystery_model,
-              "Romance": romance_model,
-              "Thriller": thriller_model}
-    with graph.as_default():
-        predictions = []
-        results = []
-        for genre, model in models.items():
+    predictions = []
+    results = {}
+    for genre, model in models.items():
+        if model is not None:
             value = model.predict(sequence)[0][0]
             if value > 0.5:
                 predictions.append(genre)
-            results.append({genre:value})
-        print(results)
-        return (predictions, results)
+            results[genre] = str(value)
+        else:
+            print("What")
+    print(results)
+    return (predictions, results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
---
+++ static/js/results.js
@@ -0,0 +1,36 @@
+ $('#submit').click( function(e) {
+     e.preventDefault();
+     console.log("Get JSON");
+     $.getJSON($SCRIPT_ROOT + '/submit', {
+         text: $('textarea[name="text"]').val()
+     }, function (data) {
+         $("#action-td").text(data.result.Action);
+         displayBadge(data.result.Action, $("#action-badge"));
+         $("#adventure-td").text(data.result.Adventure);
+         displayBadge(data.result.Adventure, $("#adventure-badge"));
+         $("#comedy-td").text(data.result.Comedy);
+         displayBadge(data.result.Comedy, $("#comedy-badge"));
+         $("#crime-td").text(data.result.Crime);
+         displayBadge(data.result.Crime, $("#crime-badge"));
+         $("#family-td").text(data.result.Family);
+         displayBadge(data.result.Family, $("#family-badge"));
+         $("#mystery-td").text(data.result.Mystery);
+         displayBadge(data.result.Mystery, $("#mystery-badge"));
+         $("#romance-td").text(data.result.Romance);
+         displayBadge(data.result.Romance, $("#romance-badge"));
+         $("#thriller-td").text(data.result.Thriller);
+         displayBadge(data.result.Thriller, $("#thriller-badge"));
+         console.log("Complete");
+     });
+     return false;
+ });
+
+function displayBadge(data, id) {
+     if (parseFloat(data) >= 0.5) {
+        console.log("display");
+         id.css('display', 'inline');
+     } else {
+         console.log("not display");
+        id.css('display', 'none');
+     }
+}
