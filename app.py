2b894f4067e4ba71c76ab2021f5faae0be3f1f03
TamPhilip <philip.tam.code@gmail.com>
2019-04-10 20:12:23 -0400

    Changed format

--- app.py
+++ app.py
@@ -5,28 +5,34 @@ from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 import json
 #
-# print("Here")
-# action_model = load_model('./resources/n_most/action_model.h5')
-# print("Second")
-# adventure_model = load_model('./resources/n_most/adventure_model.h5')
-# print("Seconds")
-# comedy_model = load_model('./resources/n_most/comedy_model.h5')
-# print("Foyurth")
-# crime_model = load_model('./resources/n_most/crime_model.h5')
-# family_model = load_model('./resources/n_most/family_model.h5')
-# print("sa")
-# mystery_model = load_model('./resources/n_most/mystery_model.h5')
-# romance_model = load_model('./resources/n_most/romance_model.h5')
-# thriller_model = load_model('./resources/n_most/thriller_model.h5')
-#
+
+action_model = load_model('./resources/n_most/action_model.h5')
+action_model._make_predict_function()
+adventure_model = load_model('./resources/n_most/adventure_model.h5')
+adventure_model._make_predict_function()
+comedy_model = load_model('./resources/n_most/comedy_model.h5')
+comedy_model._make_predict_function()
+crime_model = load_model('./resources/n_most/crime_model.h5')
+crime_model._make_predict_function()
+family_model = load_model('./resources/n_most/family_model.h5')
+family_model._make_predict_function()
+mystery_model = load_model('./resources/n_most/mystery_model.h5')
+mystery_model._make_predict_function()
+romance_model = load_model('./resources/n_most/romance_model.h5')
+romance_model._make_predict_function()
+thriller_model = load_model('./resources/n_most/thriller_model.h5')
+thriller_model._make_predict_function()
+
+print("Done Loading")
 # models = {"Action": action_model,
-#           "Adventure": adventure_model,
-#           'Comedy': comedy_model,
-#           "Crime": crime_model,
+          # "Adventure": adventure_model,
+          # 'Comedy': comedy_model,
+          # "Crime": crime_model,
 #           "Family": family_model,
 #           "Mystery": mystery_model,
-#           "Romance": romance_model,
-#           "Thriller": thriller_model}
+#           "Romance": romance_model
+#           "Thriller": thriller_model
+#             }
 #
 # del action_model
 # del adventure_model
@@ -48,7 +54,8 @@ def main():
 
 @app.route('/submit', methods = ['GET'])
 def submit():
-    text = request.args.get('text', "", type=str)
+    text = request.args.get('text', "", type=str).lstrip()
+    text = text.replace('\n', ' ')
     clean = [re.sub(r"[^a-zA-Z0-9]+", ' ', k) for k in text.lower().split("\n")]
     x = tokenize(clean)
     tags, results = predict(x)
@@ -65,6 +72,7 @@ def submit():
 @app.route('/tokenizer')
 def run_tokenizer():
     text = request.args.get('text').lstrip()
+    text = text.replace('\n', ' ')
     text = [re.sub(r"[^a-zA-Z0-9]+", ' ', k) for k in text.lower().split("\n")]
     x = tokenize(text).tolist()
     token = {'token': x}
@@ -72,8 +80,9 @@ def run_tokenizer():
 
 @app.route('/predict')
 def run_prediction():
-    text = request.args.get('text')
-    text = [re.sub(r"[^a-zA-Z0-9]+", ' ', k) for k in text.lower().split("\n")]
+    text = request.args.get('text').lstrip()
+    text = text.replace('\n', ' ')
+    text = [re.sub("[^a-zA-Z0-9]+", ' ', k) for k in text.lower().split("\n")]
     x = tokenize(text)
     return Response(json.dumps({'prediction': predict(x)}), mimetype='application/json')
 
@@ -84,17 +93,34 @@ def tokenize(text):
         x = pad_sequences(sequence, maxlen=500)
         return x
 
+
+def do_pred(model, genre, sequence, predictions, results):
+    value = model.predict(sequence)[0][0]
+    if value > 0.5:
+        predictions.append(genre)
+    results[genre] = str(value)
+    return predictions, results
+
+
 def predict(sequence):
     predictions = []
     results = {}
-    for genre, model in models.items():
-        if model is not None:
-            value = model.predict(sequence)[0][0]
-            if value > 0.5:
-                predictions.append(genre)
-            results[genre] = str(value)
-        else:
-            print("What")
+    predictions, results = do_pred(action_model, "Action", sequence, predictions, results)
+    predictions, results = do_pred(adventure_model, "Adventure", sequence, predictions, results)
+    predictions, results = do_pred(comedy_model, "Comedy", sequence, predictions, results)
+    predictions, results = do_pred(crime_model, "Crime", sequence, predictions, results)
+    predictions, results = do_pred(family_model, "Family", sequence, predictions, results)
+    predictions, results = do_pred(mystery_model, "Mystery", sequence, predictions, results)
+    predictions, results = do_pred(romance_model, "Romance", sequence, predictions, results)
+    predictions, results = do_pred(thriller_model, "Thriller", sequence, predictions, results)
+    # for genre, model in models.items():
+    #     if model is not None:
+    #         value = model.predict(sequence)[0][0]
+    #         if value > 0.5:
+    #             predictions.append(genre)
+    #         results[genre] = str(value)
+    #     else:
+    #         print("What")
     print(results)
     return (predictions, results)
 
